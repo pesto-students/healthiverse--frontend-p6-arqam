87213d0da3a87e223bc97c1685e3736cc82a5ec9
manikdevbhagat <manikdevbhagat@gmail.com>
2023-03-23 10:24:23 +0530

    Added redux persist to store

--- src/store.js
+++ src/store.js
@@ -1,23 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 import authReducer from "./slices/auth";
 import messageReducer from "./slices/message";
-import subscriberReducer from "./slices/subscriber";
-import businessReducer from "./slices/business";
-import businessAndMembershipReducer from "./slices/businessMembership";
+import subscriberReducer from "./slices/subscriberProfile";
+import businessReducer from "./slices/businessProfile";
 import chatRoomReducer from "./slices/chatRooms";
+import browseBusinessReducer from "./slices/browseBusiness";
+import membershipReducer from "./slices/membership";
 
-const reducer = {
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const reducer = combineReducers({
     auth: authReducer,
     message: messageReducer,
     subscriber: subscriberReducer,
     business: businessReducer,
-    businessAndMembership: businessAndMembershipReducer,
+    browseBusiness: browseBusinessReducer,
+    membership: membershipReducer,
     chatRoom: chatRoomReducer
-};
-
-const store = configureStore({
-    reducer: reducer,
-    devTools: true
 });
 
-export default store;
+const rootReducer = (state, action) => {
+    if (action.type === 'auth/logout') {
+        state = undefined;
+    }
+    return reducer(state, action);
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    devTools: true,
+    middleware: [thunk]
+});
+
+export const persistor = persistStore(store);
