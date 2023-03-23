0bb6ab0cd6c9ae72ff5d77e13c8c79161e06c61c
manikdevbhagat <manikdevbhagat@gmail.com>
2023-03-23 10:20:08 +0530

    Import update

--- src/components/dashboard/profile/FormSubscriber.js
+++ src/components/dashboard/profile/FormSubscriber.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { postSubscriberProfile } from "../../../slices/subscriber";
+import { postSubscriberProfile } from "../../../slices/subscriberProfile";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
