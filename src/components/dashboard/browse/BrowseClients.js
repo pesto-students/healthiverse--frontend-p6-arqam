dd9ba0adc172e423b6c346b3a464f5fdb25a2d8c
manikdevbhagat <manikdevbhagat@gmail.com>
2023-03-26 14:27:11 +0530

    Wrote function to separate client types

--- src/components/dashboard/browse/BrowseClients.js
+++ src/components/dashboard/browse/BrowseClients.js
@@ -3,20 +3,42 @@ import userService from "../../../services/user.service";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setRoom } from "../../../slices/chatRooms";
-import socket from "../../../socket/socket";
 
 const BrowseClients = () => {
     const [clients, setClients] = useState([]);
+    const gymClients = [];
+    const trainerClients = [];
+    const dieticianClients = [];
+
+    for (const client of clients) {
+        let gymMembership = client.membership.filter(m => m.businessType === "gym");
+        let trainerMembership = client.membership.filter(m => m.businessType === "trainer");
+        let dieticianMembership = client.membership.filter(m => m.businessType === "dietician");
+        for (const el of gymMembership) {
+            let obj = { ...client, businessId: el.businessId, endDate: el.endDate };
+            if (obj) gymClients.push(obj);
+        }
+        for (const el of trainerMembership) {
+            let obj = { ...client, businessId: el.businessId, endDate: el.endDate };
+            if (obj) trainerClients.push(obj);
+        }
+        for (const el of dieticianMembership) {
+            let obj = { ...client, businessId: el.businessId, endDate: el.endDate };
+            if (obj) dieticianClients.push(obj);
+        }
+    }
+
     const [isLoading, setIsLoading] = useState(true);
     const { user: currentUser } = useSelector((state) => state.auth);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     useEffect(() => {
-        userService.getClients().then((res) => {
-            setClients(res.data);
-            setIsLoading(false);
-        })
+        userService.getClients()
+            .then((res) => {
+                setClients(res.data);
+                setIsLoading(false);
+            })
             .catch((err) => {
                 const _content = (err.response &&
                     err.response.data &&
@@ -27,10 +49,8 @@ const BrowseClients = () => {
     }, [])
 
     const chatClick = (item) => {
-        const roomId = item._id + "+" + currentUser._id;
         dispatch(setRoom({
-            roomId: roomId,
-            user1: currentUser,
+            user1: {...currentUser, senderType: "business"},
             user2: item
         }));
         // socket.emit('join_room', { name: currentUser.name, roomId: roomId });
@@ -38,30 +58,91 @@ const BrowseClients = () => {
     };
 
     return (
-        <div className="container">
-            <div className="clients">
-                <h3>Clients</h3>
-                {isLoading && <p>Loading...</p>}
+        <>
+            <div className="container">
+                <div className="clients">
+                    {(isLoading) &&
+                        (<p>Loading...</p>)}
 
-                {(!isLoading && clients.length === 0) ?
-                    (<p>No active clients</p>) :
-                    (clients.map((item, index) => {
-                        return (
-                            <div key={index}>
-                                <p>
-                                    {item.name}
-                                </p>
-                                <button onClick={() => {
-                                    chatClick(item);
-                                }}>
-                                    Chat
-                                </button>
-                            </div>
-                        );
-                    }))}
+                    {(gymClients.length > 0) ?
+                        (<div>
+                            <h3>Gym Clients</h3>
+                            {gymClients.map(item => {
+                                return (
+                                    <div key={item._id}>
+                                        <p>
+                                            {item.name}
+                                        </p>
+                                        <p>
+                                            End Date: {item.endDate}
+                                        </p>
+                                        <button onClick={() => {
+                                            chatClick(item);
+                                        }}>
+                                            Chat
+                                        </button>
+                                    </div>
+                                )
+                            })}
+                        </div>) :
+                        (<></>)
+                    }
+
+                    {(trainerClients.length > 0) ?
+                        (<div>
+                            <h3>Trainer Clients</h3>
+                            {trainerClients.map(item => {
+                                return (
+                                    <div key={item._id}>
+                                        <p>
+                                            {item.name}
+                                        </p>
+                                        <p>
+                                            End Date: {item.endDate}
+                                        </p>
+                                        <button onClick={() => {
+                                            chatClick(item);
+                                        }}>
+                                            Chat
+                                        </button>
+                                    </div>
+                                )
+                            })}
+                        </div>) :
+                        (<></>)
+                    }
+
+                    {(dieticianClients.length > 0) ?
+                        (<div>
+                            <h3>Dietician Clients</h3>
+                            {dieticianClients.map(item => {
+                                return (
+                                    <div key={item._id}>
+                                        <p>
+                                            {item.name}
+                                        </p>
+                                        <p>
+                                            End Date: {item.endDate}
+                                        </p>
+                                        <button onClick={() => {
+                                            chatClick(item);
+                                        }}>
+                                            Chat
+                                        </button>
+                                    </div>
+                                )
+                            })}
+                        </div>) :
+                        (<></>)
+                    }
+
+                    {(!isLoading && gymClients.length === 0 && trainerClients.length === 0 && dieticianClients.length === 0) ?
+                        (<p>No active clients</p>) :
+                        (<></>)}
+
+                </div>
             </div>
-        </div>
+        </>
     );
 }
-
 export default BrowseClients;
