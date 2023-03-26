f2cd5525f8acbbe968cc14784c3053d71e718dc0
manikdevbhagat <manikdevbhagat@gmail.com>
2023-03-26 15:03:45 +0530

    Removed role from data object

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,11 +4,11 @@ import PORT from "./port";
 const API_URL = PORT + "/api/users/";
 
 const register = (data) => {
-    const { name, email, password, role } = data;
+    const { name, email, password } = data;
     console.log(API_URL);
     return axios
         .post(API_URL + "register/", {
-            name, email, password, role
+            name, email, password
         });
 };
 
@@ -20,10 +20,8 @@ const login = (data) => {
         .then((res) => {
             if (res.data.token) {
                 const _user = {
-                    _id: res.data._id,
                     name: res.data.name,
                     email: res.data.email,
-                    role: res.data.role,
                     token: res.data.token,
                 }
                 localStorage.setItem("user", JSON.stringify(_user));
