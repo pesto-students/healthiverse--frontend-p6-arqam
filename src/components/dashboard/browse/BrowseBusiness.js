0c029c4e6800c293e5f70d8918c76e660b3c071f
manikdevbhagat <manikdevbhagat@gmail.com>
2023-04-02 13:30:24 +0530

    Display max 2 businesses in each catergory

--- src/components/dashboard/browse/BrowseBusiness.js
+++ src/components/dashboard/browse/BrowseBusiness.js
@@ -5,9 +5,9 @@ import { getAllBusiness } from "../../../slices/browseBusiness";
 
 const BrowseBusiness = () => {
     const { isLoading, allBusiness } = useSelector((state) => state.browseBusiness);
-    const gyms = allBusiness?.filter(business=>business.businessType==="gym");
-    const trainers = allBusiness?.filter(business=>business.businessType==="trainer");
-    const dieticians = allBusiness?.filter(business=>business.businessType==="dietician");
+    const gyms = allBusiness?.filter(business => business.businessType === "gym");
+    const trainers = allBusiness?.filter(business => business.businessType === "trainer");
+    const dieticians = allBusiness?.filter(business => business.businessType === "dietician");
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -31,26 +31,27 @@ const BrowseBusiness = () => {
         <div className="container">
             <div className="gyms">
                 <h3>Gyms</h3>
+                {(!isLoading) && gyms.length > 0 && <Link to="gyms">see all</Link>}
                 {isLoading ?
                     (<p>Loading...</p>) :
                     ((gyms?.length === 0) ?
                         (<p>No gyms found</p>) :
-                        (gyms?.map((item, index) => {
-                            return (<li key={index}><Link to={item._id}>{item.name}</Link></li>)
+                        (gyms?.slice(0, 2).map((item, index) => {
+                            return (<li key={index}><Link to={`gyms/${item._id}`}>{item.name}</Link></li>)
                         })
                         )
                     )
                 }
-
             </div>
             <div className="trainers">
                 <h3>Trainers</h3>
+                {(!isLoading) && trainers.length > 0 && <Link to="gyms">see all</Link>}
                 {isLoading ?
                     (<p>Loading...</p>) :
                     ((trainers?.length === 0) ?
                         (<p>No trainers found</p>) :
-                        (trainers?.map((item, index) => {
-                            return (<li key={index}><Link to={item._id}>{item.name}</Link></li>)
+                        (trainers?.slice(0, 2).map((item, index) => {
+                            return (<li key={index}><Link to={`trainers/${item._id}`}>{item.name}</Link></li>)
                         })
                         )
                     )
@@ -58,12 +59,13 @@ const BrowseBusiness = () => {
             </div>
             <div className="dieticians">
                 <h3>Dieticians</h3>
+                {(!isLoading) && dieticians.length > 0 && <Link to="gyms">see all</Link>}
                 {isLoading ?
                     (<p>Loading...</p>) :
                     ((dieticians?.length === 0) ?
                         (<p>No dieticians found</p>) :
-                        (dieticians?.map((item, index) => {
-                            return (<li key={index}><Link to={item._id}>{item.name}</Link></li>)
+                        (dieticians?.slice(0, 2).map((item, index) => {
+                            return (<li key={index}><Link to={`dieticians/${item._id}`}>{item.name}</Link></li>)
                         })
                         )
                     )
