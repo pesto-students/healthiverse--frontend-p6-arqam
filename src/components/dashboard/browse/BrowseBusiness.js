d877d89ea7bb68a84e324528bf6a9250705b0f69
manikdevbhagat <manikdevbhagat@gmail.com>
2023-04-05 01:39:25 +0530

    Replaced link with button

--- src/components/dashboard/browse/BrowseBusiness.js
+++ src/components/dashboard/browse/BrowseBusiness.js
@@ -25,22 +25,22 @@ const BrowseBusiness = () => {
                     console.log(_content);
                 });
         }
-
-        fetchData();
-
+        if (!allBusiness) {
+            fetchData();
+        }
     }, []);
 
     return (
         <div className="container">
             <div className="gyms">
                 <h3>Gyms</h3>
-                {(!isLoading) && gyms?.length > 0 && <Link to="gyms">see all</Link>}
+                {(!isLoading) && gyms?.length > 0 && <button onClick={()=>navigate("gym")}>see all</button>}
                 {isLoading ?
                     (<p>Loading...</p>) :
                     ((gyms?.length === 0) ?
                         (<p>No gyms found</p>) :
                         (gyms?.slice(0, 2).map((item, index) => {
-                            return (<div key={index} style={{ width: "300px", backgroundColor: "grey", margin: "10px", cursor: "pointer" }} onClick={() => navigate(`gyms/${item._id}`)} >
+                            return (<div key={index} style={{ width: "300px", backgroundColor: "grey", margin: "10px", cursor: "pointer" }} onClick={() => navigate(`gym/${item._id}`)} >
                                 <div><Avatar src={item.userImage} style={{ width: "50px", height: "50px" }} /></div>
                                 <div><strong>{item.name}</strong></div>
                                 <div>
@@ -66,13 +66,13 @@ const BrowseBusiness = () => {
             </div>
             <div className="trainers">
                 <h3>Trainers</h3>
-                {(!isLoading) && trainers?.length > 0 && <Link to="trainers">see all</Link>}
+                {(!isLoading) && trainers?.length > 0 && <button onClick={()=>navigate("trainer")}>see all</button>}
                 {isLoading ?
                     (<p>Loading...</p>) :
                     ((trainers?.length === 0) ?
                         (<p>No trainers found</p>) :
                         (trainers?.slice(0, 2).map((item, index) => {
-                            return (<div key={index} style={{ width: "300px", backgroundColor: "grey", margin: "10px", cursor: "pointer" }} onClick={() => navigate(`gyms/${item._id}`)} >
+                            return (<div key={index} style={{ width: "300px", backgroundColor: "grey", margin: "10px", cursor: "pointer" }} onClick={() => navigate(`trainer/${item._id}`)} >
                                 <div><Avatar src={item.userImage} style={{ width: "50px", height: "50px" }} /></div>
                                 <div><strong>{item.name}</strong></div>
                                 <div>
@@ -98,13 +98,13 @@ const BrowseBusiness = () => {
             </div>
             <div className="dieticians">
                 <h3>Dieticians</h3>
-                {(!isLoading) && dieticians?.length > 0 && <Link to="dieticians">see all</Link>}
+                {(!isLoading) && dieticians?.length > 0 && <button onClick={()=>navigate("dietician")}>see all</button>}
                 {isLoading ?
                     (<p>Loading...</p>) :
                     ((dieticians?.length === 0) ?
                         (<p>No dieticians found</p>) :
                         (dieticians?.slice(0, 2).map((item, index) => {
-                            return (<div key={index} style={{ width: "300px", backgroundColor: "grey", margin: "10px", cursor: "pointer" }} onClick={() => navigate(`gyms/${item._id}`)} >
+                            return (<div key={index} style={{ width: "300px", backgroundColor: "grey", margin: "10px", cursor: "pointer" }} onClick={() => navigate(`dietician/${item._id}`)} >
                                 <div><Avatar src={item.userImage} style={{ width: "50px", height: "50px" }} /></div>
                                 <div><strong>{item.name}</strong></div>
                                 <div>
