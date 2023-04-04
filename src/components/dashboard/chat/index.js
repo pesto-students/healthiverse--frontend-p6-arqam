f6a942ba492c440a1389d524b8635213e837e7d3
manikdevbhagat <manikdevbhagat@gmail.com>
2023-04-05 01:38:48 +0530

    Added button to go back

--- src/components/dashboard/chat/index.js
+++ src/components/dashboard/chat/index.js
@@ -2,18 +2,17 @@ import styles from './styles.module.css';
 import Header from './header';
 import MessagesReceived from './messagesReceived';
 import SendMessage from './sendMessage';
-// import socket from '../../../socket/socket';
-// import { socket } from '../profile/Profile';
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import io from 'socket.io-client';
 import PORT from '../../../services/port';
+import { useNavigate } from 'react-router-dom';
 
 
 const Chat = () => {
     const { user: currentUser } = useSelector(state => state.auth);
     const { user1, user2 } = useSelector(state => state.chatRoom);
-
+    const navigate = useNavigate();
     let socket = io.connect(PORT, {
         query: { token: currentUser.token }
     });
@@ -24,6 +23,7 @@ const Chat = () => {
 
     return (
         <div className={styles.chatContainer}>
+            <button onClick={() => navigate(-1)}>go back</button>
             <Header />
             <div>
                 <MessagesReceived socket={socket}/>
