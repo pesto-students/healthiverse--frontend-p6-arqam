9393da78553905c773436fa1a19a8959ac1a3cd5
manikdevbhagat <manikdevbhagat@gmail.com>
2023-04-05 01:37:25 +0530

    Included business activities

--- src/components/dashboard/profile/BusinessProfileDetails.js
+++ src/components/dashboard/profile/BusinessProfileDetails.js
@@ -41,8 +41,16 @@ const BusinessProfileDetails = () => {
                 <strong>About:</strong> {business?.about}
             </p>
             <p>
-                <strong>Adress:</strong> {business?.address}
+                <strong>Activities:</strong>
+                <ul>
+                    {business?.activities.map((item) => {
+                        return <li>{item}</li>
+                    })}
+                </ul>
             </p>
+            {business?.businessType === "gym" && <p>
+                <strong>Address:</strong> {business?.address}
+            </p>}
             <p>
                 <strong>Contact:</strong> {business?.contact}
             </p>
