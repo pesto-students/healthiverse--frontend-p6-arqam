0fb4182bed4654c275bf7baa07aa601e840db33f
manikdevbhagat <manikdevbhagat@gmail.com>
2023-04-12 18:33:41 +0530

    material-tailwind installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import withMT from "@material-tailwind/react/utils/withMT";
+module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+});
