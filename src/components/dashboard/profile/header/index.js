b9ff63db56fbc70ba6a93dd0c817746f091d331d
manikdevbhagat <manikdevbhagat@gmail.com>
2023-04-24 01:40:05 +0530

    Added "about" info to profile header

--- src/components/dashboard/profile/header/index.js
+++ src/components/dashboard/profile/header/index.js
@@ -1,20 +1,20 @@
+import { CardBody, Typography } from "@material-tailwind/react";
 import { Avatar } from "@mui/material";
 
 const ProfileHeader = ({ item }) => {
     return (
-        <>
-            <div className="flex justify-center content-center">
-                <Avatar
-                    alt="Avatar"
-                    src={item?.userImage}
-                    className="h-40 w-40"
-                />
-            </div>
-            <div className="text-3xl font-bold text-center mt-2 mb-2">
-                {item?.name}
-            </div>
-        </>
-    )
+      <>
+        <div className="flex justify-center content-center">
+          <Avatar alt="Avatar" src={item?.userImage} className="h-40 w-40" />
+        </div>
+        <CardBody className="text-center">
+          <Typography variant="h5" className="mb-2">
+            {item.name}
+          </Typography>
+          <Typography>{item.about}</Typography>
+        </CardBody>
+      </>
+    );
 }
 
 export default ProfileHeader;
