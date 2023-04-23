b61334025372e2ae33702f53063a8307d12f9fb4
manikdevbhagat <manikdevbhagat@gmail.com>
2023-04-24 01:40:24 +0530

    CSS updates

--- src/components/dashboard/profile/info/index.js
+++ src/components/dashboard/profile/info/index.js
@@ -4,8 +4,8 @@ const ProfileInfo = ({ items }) => {
             {items.map(item => {
                 return (
                     <div className="w-full mt-2 flex">
-                        <div className="w-max text-lg font-bold">{item.heading}</div>
-                        <div className="w-80 text-lg ml-2 break-words flex flex-wrap">
+                        <div className="w-max text-sm font-bold">{item.heading}</div>
+                        <div className="w-full text-sm ml-2 break-words flex flex-wrap">
                             {item.value}
                         </div>
                     </div>
